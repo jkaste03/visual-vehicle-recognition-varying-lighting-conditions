0ebab5390fff03ce1bb01be457467cd17ea0e9c4
John Oscar <143664916+139727@users.noreply.github.com>
2026-02-13 12:02:18 +0100

    Fikset et par ting i dataanalyse. Første versjon av innlesing av bilder

---
+++ utils.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from typing import Tuple
+import os
+import tensorflow as tf
+from keras.utils import load_img, img_to_array
+import sys
+import matplotlib.pyplot as plt
+import keras
+import re
+
+internal = pd.read_csv('annotations/internal.csv')
+external = pd.read_csv('annotations/external.csv')
+
+
+def image_label_to_array(image_label: str, target_size: Tuple[int, int]):
+    image_name = image_label.split("/")[-1].split("5C")[-1]
+    image_name = re.sub(r"^.{8}-", "", image_name)
+    print(image_name)
+    path = os.path.join("datasett", image_name)
+    print(path)
+    img = load_img(path, target_size=target_size,
+                   interpolation="bicubic", keep_aspect_ratio=True)
+    img = img_to_array(img)/255
+    return img
+
+
+def read_internal_data(target_size: Tuple[int, int]) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    internal_labels = internal.drop("image", axis=1)
+    internal_images = pd.DataFrame(
+        [image_label_to_array(i, target_size=target_size)
+         for i in internal["image"]]
+    )
+    return (internal_labels, internal_images)
+
+
+if __name__ == '__main__':
+    imgs = []
+
+    for img in internal["image"]:
+        imgs.append(image_label_to_array(img, (300, 300)))
+    print(len(imgs))
+    print(sys.getsizeof(imgs))
