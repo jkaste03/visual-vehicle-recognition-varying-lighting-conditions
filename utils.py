aa2b93cc1895dc1cb71cdd3ed971018a46cc3ac0
John Oscar <143664916+139727@users.noreply.github.com>
2026-02-13 15:44:25 +0100

    Utils laster og splitter data i test og treningssett

--- utils.py
+++ utils.py
@@ -1,47 +1,74 @@
+import numpy as np
 import pandas as pd
 from typing import Tuple
 import os
 from keras.utils import load_img, img_to_array
-import sys
 import re
-import matplotlib.pyplot as plt
-
+from sklearn.model_selection import train_test_split
 
 internal = pd.read_csv('annotations/internal.csv')
 external = pd.read_csv('annotations/external.csv')
+default = internal["image"][0]
 
 
 def image_label_to_array(image_label: str, target_size: Tuple[int, int]):
     image_name = image_label.split("/")[-1].split("5C")[-1]
     image_name = re.sub(r"^.{8}-", "", image_name)
     path = os.path.join("datasett", image_name)
-    img = load_img(path, target_size=target_size,
-                   interpolation="bicubic", keep_aspect_ratio=True)
-    img = img_to_array(img)/255
-    return img
+    if (os.path.exists(path)):
+        img = load_img(
+            path,
+            target_size=target_size,
+            interpolation="bicubic",
+            color_mode="rgb",
+            keep_aspect_ratio=True
+        )
+
+        img = img_to_array(img) / 255.0
+
+        return img
+    return None
 
 
-def read_internal_data(target_size: Tuple[int, int]) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    internal_labels = internal.drop("image", axis=1)
-    internal_images = pd.DataFrame(
-        [image_label_to_array(i, target_size=target_size)
-         for i in internal["image"]]
-    )
-    return (internal_labels, internal_images)
+# def read_internal_data(target_size: Tuple[int, int], data=internal) -> Tuple[np.ndarray, pd.DataFrame]:
+#     internal_labels = data.drop("image", axis=1)
+
+#     internal_images = np.array([
+#         image_label_to_array(i, target_size=target_size)
+#         for i in internal["image"]
+#     ])
+
+#     return (internal_images, internal_labels)
+
+
+def read_and_split(target_size: Tuple[int, int], data=internal) -> Tuple[np.ndarray, pd.DataFrame]:
+    images_list = []
+    valid_indices = []
+
+    for idx, img_label in enumerate(data["image"]):
+        img = image_label_to_array(img_label, target_size=target_size)
+
+        if img is not None:
+            images_list.append(img)
+            valid_indices.append(idx)
+
+    external_images = np.array(images_list)
+    external_labels = data.iloc[valid_indices].drop("image", axis=1)
+    return (external_images, external_labels)
+
+
+def read_data():
+    combined = pd.concat([internal, external])
+    train_set, test_set = train_test_split(
+        combined, test_size=0.15, stratify=combined['model'])
+    train_x, train_y = read_and_split(
+        target_size=(300, 300), data=train_set)
+    test_x, test_y = read_and_split(target_size=(300, 300), data=test_set)
+    return (train_x, train_y), (test_x, test_y)
 
 
 def main():
-    import time
-    start_time = time.perf_counter()
-
-    imgs = []
-    for img in internal["image"]:
-        imgs.append(image_label_to_array(img, (300, 300)))
-    print(len(imgs))
-    print(sys.getsizeof(imgs))
-    end_time = time.perf_counter()
-    elapsed_time = end_time - start_time
-    print(f"Execution time: {elapsed_time:.4f} seconds")
+    read_data()
 
 
 if __name__ == '__main__':
