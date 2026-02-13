a6f2c233dd19c8e703acac4188e56cf4dfcf50f5
John Oscar <143664916+139727@users.noreply.github.com>
2026-02-13 21:53:20 +0100

    Fullførte første versjon av utils. Har lagd en veldig enkel model for å teste at dataene fungerer korrekt.

--- utils.py
+++ utils.py
@@ -8,12 +8,24 @@ from sklearn.model_selection import train_test_split
 
 internal = pd.read_csv('annotations/internal.csv')
 external = pd.read_csv('annotations/external.csv')
+internal["year"] = internal[["year_3", "year_s", "year_x", "year_y"]].bfill(
+    axis="columns").iloc[:, 0]
+external["year"] = external[["year_3", "year_s", "year_x", "year_y"]].bfill(
+    axis="columns").iloc[:, 0]
+
+internal.drop(["year_3", "year_s", "year_x", "year_y"], axis=1, inplace=True)
+external.drop(["year_3", "year_s", "year_x", "year_y"], axis=1, inplace=True)
+
 default = internal["image"][0]
 
 
 def image_label_to_array(image_label: str, target_size: Tuple[int, int]):
     image_name = image_label.split("/")[-1].split("5C")[-1]
     image_name = re.sub(r"^.{8}-", "", image_name)
+    image_name = re.sub(r".webp", ".jpg", image_name)
+    if ("%25~" in image_name):
+        image_name = re.sub(r"25", "", image_name, count=1)
+
     path = os.path.join("datasett", image_name)
     if (os.path.exists(path)):
         img = load_img(
@@ -27,18 +39,8 @@ def image_label_to_array(image_label: str, target_size: Tuple[int, int]):
         img = img_to_array(img) / 255.0
 
         return img
+    print(path, image_label)
     return None
-
-
-# def read_internal_data(target_size: Tuple[int, int], data=internal) -> Tuple[np.ndarray, pd.DataFrame]:
-#     internal_labels = data.drop("image", axis=1)
-
-#     internal_images = np.array([
-#         image_label_to_array(i, target_size=target_size)
-#         for i in internal["image"]
-#     ])
-
-#     return (internal_images, internal_labels)
 
 
 def read_and_split(target_size: Tuple[int, int], data=internal) -> Tuple[np.ndarray, pd.DataFrame]:
@@ -57,7 +59,7 @@ def read_and_split(target_size: Tuple[int, int], data=internal) -> Tuple[np.ndar
     return (external_images, external_labels)
 
 
-def read_data():
+def read_data() -> Tuple[Tuple[np.ndarray, pd.DataFrame], Tuple[np.ndarray, pd.DataFrame]]:
     combined = pd.concat([internal, external])
     train_set, test_set = train_test_split(
         combined, test_size=0.15, stratify=combined['model'])
@@ -65,6 +67,21 @@ def read_data():
         target_size=(300, 300), data=train_set)
     test_x, test_y = read_and_split(target_size=(300, 300), data=test_set)
     return (train_x, train_y), (test_x, test_y)
+
+
+def read_gate_one_data() -> Tuple[Tuple[np.ndarray, pd.DataFrame], Tuple[np.ndarray, pd.DataFrame]]:
+    (train_x, train_y), (test_x, test_y) = read_data()
+
+    def t(x):
+        if x == "Other car":
+            return 1
+        return 0
+
+    vt = np.vectorize(t)
+
+    train_y_encoded = vt(train_y["model"])
+    test_y_encoded = vt(test_y["model"])
+    return (train_x, train_y_encoded), (test_x, test_y_encoded)
 
 
 def main():
